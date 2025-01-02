371dad839f0414b56078c87eb6aab8c831819a03
pavlen5kiy <150275009+pavlen5kiy@users.noreply.github.com>
2025-01-02 16:38:02 +0300

    Working on playability

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ FPS = 120
 pygame.display.init()
 screen_info = pygame.display.Info()
 
-WIDTH, HEIGHT = 320, 180
+WIDTH, HEIGHT = 320, 200
 
 
 def fit_aspect_ratio(screen_width, screen_height,
@@ -35,7 +35,7 @@ SCALE = screen_width / WIDTH
 CENTER = pygame.Vector2(screen_width // 2, screen_height // 2)
 
 TILE_SIZE = 8
-MAP_WIDTH, MAP_HEIGHT = 48, 48
+MAP_WIDTH, MAP_HEIGHT = 40, 25  # 48 x 48
 MAP_SIZE = TILE_SIZE * MAP_WIDTH * SCALE, TILE_SIZE * MAP_HEIGHT * SCALE
 
 GROWTH_MIN = 3600
